4249914abf7ea80c71d3046b27ca2da76b126fc7
rajdeep-paul16 <rajdeeppaul1614@gmail.com>
2023-08-18 00:01:47 +0530

    lab codes

---
+++ java codes/bitwise.java
@@ -0,0 +1,12 @@
+public class bitwise {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 10;
+        x = x<<2;
+        System.out.println(x);
+        y = y>>2;
+        System.out.println(y);
+        y = y>>>2;
+        System.out.println(y);
+    }
+}
